c293088160e8c6f98227bcb838abc7b9772eafa8
Maks <krivorukov2007@mail.ru>
2024-06-22 22:06:19 +0300

    added post data from form on js

--- js/blocks/form.js
+++ js/blocks/form.js
@@ -1,5 +1,34 @@
 const form = () => {
+    const inputRadios = document.getElementsByName('contact-radio')
+    const inputName = document.querySelector('.input-name')
+    const inputEmail = document.querySelector('.input-email')
+    const inputMessage = document.querySelector('.input-message')
 
+    const form = document.querySelector('.contact__form')
+
+    console.log(inputRadios.value);
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        let radioVal = ''
+
+        for(let val of inputRadios) {
+            if(val.checked) {
+                radioVal = val.value
+                break
+            }
+        }
+        
+
+        const data = {
+            radioVal,
+            name: inputName.value,
+            email: inputEmail.value,
+            message: inputMessage.value
+        }
+
+        console.log(data);
+    })
 }
 
-export default from
+export default form
--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 
 import banner from './blocks/banner.js'
 import slider from './blocks/slider.js'
+import form from './blocks/form.js'
 
 window.addEventListener('DOMContentLoaded', () => {
     
     banner()
     slider()
+    form()
 })
